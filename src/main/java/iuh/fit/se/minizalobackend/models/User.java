baf3df20d4242877b13599fc85cd454c21007837
congy1344 <huynhcongy2004@gmail.com>
2026-02-24 11:19:37 +0700

    fix nhắn tin web

--- src/main/java/iuh/fit/se/minizalobackend/models/User.java
+++ src/main/java/iuh/fit/se/minizalobackend/models/User.java
@@ -44,7 +44,7 @@ public class User {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
