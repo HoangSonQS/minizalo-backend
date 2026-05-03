c0803c8fdf584fb807389774abd594ae3acc4b12
xuantruong121 <luamoi2014@gmail.com>
2026-05-03 09:21:15 +0700

    feat: implement ChatController to handle real-time messaging features and chat room management

--- src/main/java/iuh/fit/se/minizalobackend/dtos/request/TypingIndicatorRequest.java
+++ src/main/java/iuh/fit/se/minizalobackend/dtos/request/TypingIndicatorRequest.java
@@ -1,5 +1,6 @@
 package iuh.fit.se.minizalobackend.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TypingIndicatorRequest {
     private String roomId;
+    
+    @JsonProperty("isTyping")
     private boolean isTyping;
+
+    @JsonProperty("isTyping")
+    public boolean isTyping() {
+        return isTyping;
+    }
 }
